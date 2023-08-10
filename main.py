dd75cf3739246063d980d638636c8dc17caa37a9
Daniyal <dmirza6@gatech.edu>
2023-08-10 15:32:56 -0400

    My main

--- main.py
+++ main.py
@@ -2,11 +2,20 @@ import pygame
 
 
 pygame.init()
+pygame.font.init()
 
 #COLORS
 BG = (0, 0, 0) #black
+red = (145, 70, 70)
+text = (191, 191, 191)
 
-screen = pygame.display.set_mode((400, 500))
+
+
+
+
+txtFont = pygame.font.SysFont('Times New Roman', 110)
+
+screen = pygame.display.set_mode((640, 480))
 
 pygame.display.set_caption("GhostChat")
 
@@ -21,5 +30,11 @@ while running:
         if event.type == pygame.QUIT:
             pygame.quit()
 
-    screen.fill(BG)
+
+    pygame.draw.rect(screen, red,(30, 20, 580, 140), 10)
+    text_title = txtFont.render("Ghost Chat", 1, text)
+    screen.blit(text_title, (60, 20))
+
+
+
     pygame.display.flip()
