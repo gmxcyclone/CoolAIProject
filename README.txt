d26e58ba7ce32e961df3873eef4121a5ddd4c7d6
Daniyal <dmirza6@gatech.edu>
2023-08-10 15:01:52 -0400

    bg

--- README.txt
+++ README.txt
@@ -1,2 +1,1 @@
 Me and Shet game lol cool ai
-yay i am so happy
--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import pygame
 
+
 pygame.init()
 
-bg = (0, 0, 0)
+#COLORS
+BG = (0, 0, 0) #black
 
+screen = pygame.display.set_mode((400, 500))
 
+pygame.display.set_caption("CoolAIProject")
 
+screen.fill(BG)
+
+pygame.display.flip()
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+    screen.fill(BG)
+    pygame.display.flip()
